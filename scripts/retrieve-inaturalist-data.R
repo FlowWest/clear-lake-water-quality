1d85d0151c0df85f7262ca4c8ff1f2d5ae101ff0
erodriguez <erodriguez@flowwest.com>
2021-06-17 09:47:32 -0700

    removes a leaflet part from the script that just gets the data

--- scripts/retrieve-inaturalist-data.R
+++ scripts/retrieve-inaturalist-data.R
@@ -1,6 +1,8 @@
 ## Retrieves inaturalist data on fishkill project in clearlake
 # install.packages("rinat")
 library(rinat)
+library(tidyverse)
+
 fish_kill_observations <- get_inat_obs_project("clear-lake-fish-kill-monitoring-project", type = c("observations", "info"), raw = TRUE)
 fish_kill_data <- tibble(taxon_name = fish_kill_observations$taxon.name,
                          taxon_common_name = fish_kill_observations$taxon.common_name.name, 
@@ -23,15 +25,6 @@ labels <- sprintf("<strong>Taxon:</strong> <strong>%s</strong> (%s) <br/> <stron
                   fish_kill_data$date_observed,
                   fish_kill_data$description
                   ) %>% lapply(htmltools::HTML)
-leaflet() %>%
-  addProviderTiles(providers$Esri.WorldTopoMap, group = "Map") %>% 
-  addProviderTiles(providers$Esri.WorldImagery, group = "Satellite") %>%
-  addCircleMarkers(data = fish_kill_data,  label = labels,
-                   color = "#972D15", 
-                   weight = 1.5,
-                   opacity =  1, fillOpacity = 1, 
-                   labelOptions = labelOptions(style = list("font-size" = "14px"))
-  )
 
 # Add useful summary stats to table once we decide what we want (for now just current data)
 summary_table <- fish_kill_data %>% 
